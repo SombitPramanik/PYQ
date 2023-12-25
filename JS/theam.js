af5f4b77270c1672a371460b34bfee89ed7af346
Sombit Pramanik <m.sombitpramanik@gmail.com>
2023-12-25 16:46:47 +0530

    add some basic stuff to the html and add the css & js filestructure

---
+++ JS/theam.js
@@ -0,0 +1,4 @@
+const light_mode = document.getElementById("light");
+const dark_mode = document.getElementById("dark");
+
+
---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MAKAUT PYQ -- SPP technologies </title>
+    <link rel="stylesheet" href="CSS/home.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div>home</div>
+            <div>Previous Year Questions</div>
+            <div>CA (1,2,3,4) Questions</div>
+            <div>login</div>
+            <div id="open"><img src="images/menu_open.png" alt="dark images"></div>
+            <div id="close"><img src="images/menu_close.png" alt="dark images"></div>
+            <div id="light"><img src="images/light_mode.png" alt="dark images"></div>
+            <div id="dark"><img src="images/dark_mode.png" alt="dark images"></div>
+        </nav>
+    </header>
+    <main>        
+
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+<script src="JS/theam.js"></script>
+
+</html>
+
+
+
+<?php
+
+// echo 'PHP Working !';
+
+
+?>
